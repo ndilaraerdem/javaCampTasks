7aed184162fcfd1874aab54c1605d8e9bf1d6f53
Dilara Erdem <ndilaraerdem@gmail.com>
2022-03-11 15:08:16 +0300

    Initial commit

---
+++ src/javaCampTasks/Day1Task3/Main.java
@@ -0,0 +1,10 @@
+package javaCampTasks.Day1Task3;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Merhaba Dünya");
+        System.out.println("Hello Dilara");
+        System.out.println("Merhaba Dilara");
+    }
+}
